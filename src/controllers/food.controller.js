d056b0dd41f5b3cda917a23b760e95fdd96cbafe
harsha vardhan k <98804098+thisisharshavardhan@users.noreply.github.com>
2025-03-22 13:01:33 +0000

    Refactor ingredient extraction logic into processImageLabel function

--- src/controllers/food.controller.js
+++ src/controllers/food.controller.js
@@ -5,68 +5,68 @@ import { Ingredients } from '../models/Ingredients.model.js';
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage }).single('image');
 
-const extractIngredients = (rawText)=> {
-    const commonIngredientIndicators = [
-        'ingredients:', 
-        'contains:', 
-        'made with',
-        'composed of',
-        'ingredients list',
-        '%*-',
-        '%* -',
-        '% -'
-    ];
-    const endIndicators = [
-        'nutrition facts',
-        'allergen information',
-        'may contain',
-        'manufactured by',
-        'best before',
-        'packed by',
-        'storage instructions',
-        'net weight',
-        'artificial flavouring',
-        'natural flavouring',
-        'nature identical'
-    ];
-    
-    const lowerText = rawText.toLowerCase();
-    let startIndex = -1;
-    let endIndex = rawText.length;
-
-    for (const indicator of commonIngredientIndicators) {
-        const index = lowerText.indexOf(indicator);
-        if (index !== -1) {
-            startIndex = index + indicator.length;
-            break;
-        }
-    }
-
-    if (startIndex === -1) return []; 
-
-    for (const endIndicator of endIndicators) {
-        const index = lowerText.indexOf(endIndicator, startIndex);
-        if (index !== -1 && index < endIndex) {
-            endIndex = index;
-            break;
-        }
-    }
-
-    const ingredientChunk = rawText.substring(startIndex, endIndex).trim();
-
-    const cleanedIngredients = ingredientChunk
-        .replace(/[\n\r]/g, ' ')         
-        .replace(/[*%]/g, '')             
-        .replace(/\s+/g, ' ')             
-        .split(/,|;/)                     
-        .map(item => item.trim())
-        .filter(item => item.length > 0);
-
-    return cleanedIngredients;
-}
 
 
 const processImageLabel = async (req, res) => {
+    const extractIngredients = (rawText)=> {
+        const commonIngredientIndicators = [
+            'ingredients:', 
+            'contains:', 
+            'made with',
+            'composed of',
+            'ingredients list',
+            '%*-',
+            '%* -',
+            '% -'
+        ];
+        const endIndicators = [
+            'nutrition facts',
+            'allergen information',
+            'may contain',
+            'manufactured by',
+            'best before',
+            'packed by',
+            'storage instructions',
+            'net weight',
+            'artificial flavouring',
+            'natural flavouring',
+            'nature identical'
+        ];
+        
+        const lowerText = rawText.toLowerCase();
+        let startIndex = -1;
+        let endIndex = rawText.length;
+    
+        for (const indicator of commonIngredientIndicators) {
+            const index = lowerText.indexOf(indicator);
+            if (index !== -1) {
+                startIndex = index + indicator.length;
+                break;
+            }
+        }
+    
+        if (startIndex === -1) return []; 
+    
+        for (const endIndicator of endIndicators) {
+            const index = lowerText.indexOf(endIndicator, startIndex);
+            if (index !== -1 && index < endIndex) {
+                endIndex = index;
+                break;
+            }
+        }
+    
+        const ingredientChunk = rawText.substring(startIndex, endIndex).trim();
+    
+        const cleanedIngredients = ingredientChunk
+            .replace(/[\n\r]/g, ' ')         
+            .replace(/[*%]/g, '')             
+            .replace(/\s+/g, ' ')             
+            .split(/,|;/)                     
+            .map(item => item.trim())
+            .filter(item => item.length > 0);
+    
+        return cleanedIngredients;
+    }
     try {
         upload(req, res, async (err) => {
             if (err) {
